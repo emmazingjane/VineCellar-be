ea1b88f60ae31941533bff3675e1c6b62d472010
stacief510 <staciemfraser@gmail.com>
2018-07-23 17:35:07 -0700

    set up server

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+const http = require('http');
+
+app.get('/', function(req, res) {
+	res.send('Server is working...');
+});
+
+///LISTEN HERE
+app.listen(process.env.PORT || 3001, function() {
+	console.log('This server is listening on port 3001')
+});
