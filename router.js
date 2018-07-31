dd2093efe664306261fa79042ed823e0272c75a6
stacief510 <staciemfraser@gmail.com>
2018-07-30 18:34:44 -0700

    created winery routes

--- router.js
+++ router.js
@@ -24,4 +24,17 @@ app.put('/users/:id', usersController.update);
 //delete a user
 app.delete('/users/:id', usersController.destroy);
 //see all users posts
+
+app.get('/users/:id/wineries', wineriesController.index);
+//show all wineries 
+app.post('/users/:id/wineries', wineriesController.create);
+//create a winery
+app.get('/users/:id/wineries/winery_id', wineriesController.show);
+//show one winery
+app.put('/users/:id/wineries/winery_id', wineriesController.update);
+//update a winery
+app.delete('/users/:id/wineries/winery_id', wineriesController.destroy);
+//delete a winery
+
+
 }
